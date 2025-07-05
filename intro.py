3506d9101f721f1cb2efd919283b9c6e0cc150c8
Kainatfatima711 <kainatfetima@gmail.com>
2025-07-05 14:21:05 +0500

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,15 @@
+#Printings a greetings
+print("Hello World!!")
+print()
+
+#Print your name
+print("My name is Kainat")
+print()
+
+#print your hobby
+print("My hobby is to play games")
+print()
+
+#Do a mathemetical calculation
+print("The sum of two numbers 2 and 3 is ",2 + 3)
+print()
