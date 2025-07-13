1724d8cf0463ee312adbf66461b0bd0347a8700f
Kainatfatima711 <kainatfetima@gmail.com>
2025-07-13 13:54:37 +0500

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,11 @@
+age = 13 #Integers
+print("type of a :",type(age))
+
+weight = 40.5 #Float
+print("type of b :", type(weight))
+
+name = "Kainat" #String
+print("type of name:", type(name))
+
+isRaining = True #boolean
+print("type of isRaining :", type(isRaining))
