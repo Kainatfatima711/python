e540f538afc09e6f745efe280fe54c058cb63df6
Kainatfatima711 <kainatfetima@gmail.com>
2025-07-13 14:21:59 +0500

    Add files via upload

---
+++ string.py
@@ -0,0 +1,15 @@
+text = input("Enter The text that you want!!!")
+print(text)
+print()
+
+mystring = "HelloWorld"
+
+print(len(mystring))
+print( )
+
+slicestring = mystring[5:8]
+print(slicestring)
+print( )
+
+reversedstring = mystring[::-1]
+print(reversedstring)
