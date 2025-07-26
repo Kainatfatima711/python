d186811143c095f340c72e3b371f2bbeda1f78b9
Kainatfatima711 <kainatfetima@gmail.com>
2025-07-26 14:15:09 +0500

    Add files via upload

---
+++ notequal.py
@@ -0,0 +1,30 @@
+# part 1
+
+a = 10
+b = 12
+c = 12
+
+print(a != b) # True
+print(a != c) # False
+
+
+# ==  checks if two things are equal in Python.
+# != means “not equal to” in coding.
+
+
+
+# part 2
+a = "python"
+b = "coding"
+
+if a != b :
+    print( a ,'and', b, 'are different.')
+
+
+#part 3
+
+a = 4
+b = 5
+if (a == 1) != (b == 5):
+ print('Hello')
+
