062347ef4e61d0bcf737b0768147f9117314c5dd
Kainatfatima711 <kainatfetima@gmail.com>
2025-08-02 14:12:25 +0500

    Add files via upload

---
+++ ReverseString.py
@@ -0,0 +1,8 @@
+string = input("Please enter your own String: ") #but
+string2 =('')
+
+for i in string:
+    string2 = i + string2
+
+print("\nThe original String = ", string)
+print("The Reversed String = ", string2)
