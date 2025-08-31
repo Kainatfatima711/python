5c7ef681fa6fe97736210f46ad71bbc5f52b3ad1
Kainatfatima711 <kainatfetima@gmail.com>
2025-08-31 13:46:10 +0500

    Add files via upload

---
+++ numbergame.py
@@ -0,0 +1,17 @@
+import random
+playing = True
+number = str(random.randint(10,15))
+
+print("I will generate a number from 01 to 15 , and you have to guess the number one digit at a time.")
+
+while playing:
+
+    guess = input("Give me your best guess! \n")
+
+    if number == guess:
+        print("You win the game")
+        print("The number was", number)
+        break
+
+    else:
+        print("Your guess isn't quite right , try again. \n")
