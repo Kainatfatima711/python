3401692736898beeb6b4b50b85172e5980b6d886
Kainatfatima711 <kainatfetima@gmail.com>
2025-09-21 13:50:28 +0500

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,14 @@
+my_set ={1.0 , "Hello" , (1 , 2 , 3)}
+print(my_set)
+
+my_set = set([1 , 2 , 3 , 2])
+print(my_set)
+
+num_set = set([0 , 1 , 3 , 4 , 5])
+print("Original set: ")
+print(num_set)
+print()
+
+num_set.pop()
+print("After removig the first element")
+print(num_set)
