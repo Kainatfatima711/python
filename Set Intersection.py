b0526d3c1693dd37768e1f3590982dc855da1960
Kainatfatima711 <kainatfetima@gmail.com>
2025-09-21 13:56:02 +0500

    Add files via upload

---
+++ Set Intersection.py
@@ -0,0 +1,11 @@
+setx = {"Green" , "Blue" , "Yellow"}
+sety = {"Blue" , "Yellow"}
+
+print("Original set elements: ")
+print(setx)
+print(sety)
+print()
+
+print("Intersection of two said sets: ")
+setz = setx.intersection(sety)
+print(setz)
