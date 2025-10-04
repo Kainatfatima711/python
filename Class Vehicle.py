81a1894a1dcbf631c9440ff673415464843daade
Kainatfatima711 <kainatfetima@gmail.com>
2025-10-04 13:50:28 +0500

    Add files via upload

---
+++ Class Vehicle.py
@@ -0,0 +1,14 @@
+class Vehicle:
+
+
+    def __init__(self , name , color):
+        
+
+        self.name = name
+        self.color = color
+
+modleone = Vehicle('audi' , 'blue')
+
+
+print("Model name: " , modleone.name)
+print("Model color: " , modleone.color)
