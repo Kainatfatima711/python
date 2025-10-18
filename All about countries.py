f3e83eb09c6d3919e0581879a139e3c4f8f6f2a5
Kainatfatima711 <kainatfetima@gmail.com>
2025-10-18 14:23:54 +0500

    Add files via upload

---
+++ All about countries.py
@@ -0,0 +1,21 @@
+class Australia():
+    def Captal(self):
+        print("Canberra is the capital of India")
+
+    def Language(self):
+        print("English is the most widely spoken language in India")
+
+    def type(self):
+        print("Austraila is a developed country")
+
+class USA():
+    def Capital(self):
+        print("Washington , D.C. is the  capital of USA")
+
+    def Language(self):
+        print("English is the primary language og USA")
+
+    def type(self):
+        print("USA is a developed country")
+
+
