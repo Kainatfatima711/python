97d2073305d4a521d17a2833786e2fd38455ab2f
Kainatfatima711 <kainatfetima@gmail.com>
2025-11-22 14:07:50 +0500

    Add files via upload

---
+++ Virus Detected.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import messagebox
+
+window = Tk()
+window.geometry("200x200")
+
+def msg():
+    messagebox.showwarning("Alert" , "Stop! Virus Found.")
+
+button = Button(window , text = "Scan for Virus" , command = msg)
+button.place(x = 40 , y = 80)
+ 
+window.mainloop()
